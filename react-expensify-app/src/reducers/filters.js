e6ba87591cea7be44b3e2d4e69ffe762a1883cf9
yadavanurag <anuragyadav13481@gmail.com>
2020-02-14 07:26:12 +1200

    final expensify without css, and without jest-testing

--- react-expensify-app/src/reducers/filters.js
+++ react-expensify-app/src/reducers/filters.js
@@ -1,9 +1,11 @@
+import moment from 'moment';
+
 // filter reducer
 const filterReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 const filterReducer = (state = filterReducerDefaultState, action)=>{
   switch (action.type){
